63bca503404ee77826fde71e9b607b7939d1874d
Jaewoo Kim <wodndhksk@gmail.com>
2023-12-23 17:10:57 +0900

    예제 프로젝트 만들기 v1 - 주석추가 (스프링 3.0 버전 이상 변경점)

--- src/main/java/hello/proxy/ProxyApplication.java
+++ src/main/java/hello/proxy/ProxyApplication.java
@@ -5,6 +5,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
+/**
+ * 스프링 부트 3.0부터는 `@Controller` , `@RestController` 를 사용했는데, 이렇게 하면 내부
+ *  `@Component` 를 가지고 있어서 컴포넌트 스캔의 대상이 된다.
+ *  따라서 스캔 대상의 범위를 "hello.proxy.app.v3" 과 같은 방식으로 변경해야한다.
+ */
 @Import(AppV1Config.class)
 @SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의
 public class ProxyApplication {
--- src/main/java/hello/proxy/app/v1/OrderControllerV1.java
+++ src/main/java/hello/proxy/app/v1/OrderControllerV1.java
@@ -2,8 +2,11 @@ package hello.proxy.app.v1;
 
 import org.springframework.web.bind.annotation.*;
 
-@RequestMapping// 스프링은 @Controller 또는 @RequestMapping 이 있어야 Controller 로 인식한다
-@ResponseBody
+/**
+ * 스프링 부트 3.0부터는 `@Controller` , `@RestController` 를 사용했는데, 이렇게 하면 내부
+ *  `@Component` 를 가지고 있어서 컴포넌트 스캔의 대상이 된다.
+ */
+@RestController
 public interface OrderControllerV1 {
 
     @GetMapping("/v1/request")
