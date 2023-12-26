153f871e258d3f23c404daf343299bec0cafa5f9
Jaewoo Kim <wodndhksk@gmail.com>
2023-12-26 14:38:05 +0900

    예제 프로젝트 만들기 v2
    - 인터페이스가 없는 구체 클래스 스프링 빈으로 수동 등록

--- src/main/java/hello/proxy/ProxyApplication.java
+++ src/main/java/hello/proxy/ProxyApplication.java
@@ -1,6 +1,7 @@
 package hello.proxy;
 
 import hello.proxy.config.AppV1Config;
+import hello.proxy.config.AppV2Config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
@@ -10,8 +11,8 @@ import org.springframework.context.annotation.Import;
  *  `@Component` 를 가지고 있어서 컴포넌트 스캔의 대상이 된다.
  *  따라서 스캔 대상의 범위를 "hello.proxy.app.v3" 과 같은 방식으로 변경해야한다.
  */
-@Import(AppV1Config.class)
-@SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의
+@Import({AppV1Config.class, AppV2Config.class})
+@SpringBootApplication(scanBasePackages = "hello.proxy.app.v3") //주의
 public class ProxyApplication {
 
 	public static void main(String[] args) {
