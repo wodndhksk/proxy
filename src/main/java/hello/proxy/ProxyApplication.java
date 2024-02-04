819f849f30c3ce8881f35c4e1eef64abff62183a
Jaewoo Kim <wodndhksk@gmail.com>
2024-02-04 14:43:43 +0900

    프록시 팩토리 - 적용2
    - v2구체 클래스 적용

--- src/main/java/hello/proxy/ProxyApplication.java
+++ src/main/java/hello/proxy/ProxyApplication.java
@@ -1,12 +1,7 @@
 package hello.proxy;
 
-import hello.proxy.config.AppV1Config;
-import hello.proxy.config.AppV2Config;
-import hello.proxy.config.v1_proxy.ConcreteProxyConfig;
-import hello.proxy.config.v1_proxy.InterfaceProxyConfig;
-import hello.proxy.config.v2_dynamicproxy.DynamicProxyBasicConfig;
-import hello.proxy.config.v2_dynamicproxy.DynamicProxyFilterConfig;
-import hello.proxy.config.v3_proxyfactory.ProxyFactoryConfig1;
+import hello.proxy.config.v3_proxyfactory.ProxyFactoryConfigV1;
+import hello.proxy.config.v3_proxyfactory.ProxyFactoryConfigV2;
 import hello.proxy.trace.logtrace.LogTrace;
 import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.boot.SpringApplication;
@@ -16,15 +11,16 @@ import org.springframework.context.annotation.Import;
 
 /**
  * 스프링 부트 3.0부터는 `@Controller` , `@RestController` 를 사용했는데, 이렇게 하면 내부
- *  `@Component` 를 가지고 있어서 컴포넌트 스캔의 대상이 된다.
- *  따라서 스캔 대상의 범위를 "hello.proxy.app.v3" 과 같은 방식으로 변경해야한다.
+ * `@Component` 를 가지고 있어서 컴포넌트 스캔의 대상이 된다.
+ * 따라서 스캔 대상의 범위를 "hello.proxy.app.v3" 과 같은 방식으로 변경해야한다.
  */
 //@Import({AppV1Config.class, AppV2Config.class})
 //@Import(InterfaceProxyConfig.class)
 //@Import(ConcreteProxyConfig.class)
 //@Import(DynamicProxyBasicConfig.class)
 //@Import(DynamicProxyFilterConfig.class)
-@Import(ProxyFactoryConfig1.class)
+//@Import(ProxyFactoryConfigV1.class)
+@Import(ProxyFactoryConfigV2.class)
 @SpringBootApplication(scanBasePackages = "hello.proxy.app.v3") //주의
 public class ProxyApplication {
 
---
+++ src/main/java/hello/proxy/config/v3_proxyfactory/ProxyFactoryConfigV1.java
@@ -0,0 +1,75 @@
+package hello.proxy.config.v3_proxyfactory;
+
+import hello.proxy.app.v1.*;
+import hello.proxy.config.v3_proxyfactory.advice.LogTraceAdvice;
+import hello.proxy.trace.logtrace.LogTrace;
+import lombok.extern.slf4j.Slf4j;
+import org.aopalliance.aop.Advice;
+import org.springframework.aop.framework.ProxyFactory;
+import org.springframework.aop.support.DefaultPointcutAdvisor;
+import org.springframework.aop.support.NameMatchMethodPointcut;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Slf4j
+@Configuration
+public class ProxyFactoryConfigV1 {
+
+    @Bean
+    public OrderControllerV1 orderControllerV1(LogTrace logTrace) {
+        OrderControllerV1Impl orderController = new OrderControllerV1Impl(orderServiceV1(logTrace));
+        ProxyFactory factory = new ProxyFactory(orderController);
+        factory.addAdvice(getAdvisor(logTrace));
+
+        OrderControllerV1 proxy = (OrderControllerV1) factory.getProxy();
+        proxyFactoryLog(proxy.getClass(), orderController.getClass());
+        return proxy;
+    }
+
+    @Bean
+    public OrderServiceV1 orderServiceV1(LogTrace logTrace) {
+        OrderServiceV1Impl orderService = new OrderServiceV1Impl(orderRepositoryV1(logTrace));
+        ProxyFactory factory = new ProxyFactory(orderService);
+        factory.addAdvice(getAdvisor(logTrace));
+
+        OrderServiceV1 proxy = (OrderServiceV1) factory.getProxy();
+        proxyFactoryLog(proxy.getClass(), orderService.getClass());
+        return proxy;
+    }
+
+    @Bean
+    public OrderRepositoryV1 orderRepositoryV1(LogTrace logTrace){
+        OrderRepositoryV1Impl orderRepository = new OrderRepositoryV1Impl();
+        ProxyFactory factory = new ProxyFactory(orderRepository);
+        factory.addAdvice(getAdvisor(logTrace));
+
+        OrderRepositoryV1 proxy = (OrderRepositoryV1) factory.getProxy();
+        proxyFactoryLog(proxy.getClass(), orderRepository.getClass());
+        return proxy;
+    }
+
+    /**
+     * pointcut
+     * @param logTrace
+     * @return
+     */
+    private Advice getAdvisor(LogTrace logTrace) {
+        // pointcut
+        NameMatchMethodPointcut pointcut = new NameMatchMethodPointcut();
+        pointcut.setMappedNames("request*", "order*", "save*");
+
+        //advise
+        LogTraceAdvice advice = new LogTraceAdvice(logTrace);
+        DefaultPointcutAdvisor advisor = new DefaultPointcutAdvisor(pointcut, advice);
+        return advice;
+    }
+
+    /**
+     * log 메소드
+     * @param proxy
+     * @param orderClass
+     */
+    private void proxyFactoryLog(Class<?> proxy, Class<?> orderClass) {
+        log.info("proxyFactory proxy={} target={}", proxy, orderClass);
+    }
+}
